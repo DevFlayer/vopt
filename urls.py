1e219c68110c4d28e8bddbccb84a9d0c3e122bcf
FlayerDEV <viniciusprivado16@gmail.com>
2026-06-07 15:14:31 -0300

    Adicionado a pagina de email pos cadastro

--- urls.py
+++ urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("cadastro/", TemplateView.as_view(template_name="cadastro.html"), name="cadastro"),
     path("recuperar-senha/", TemplateView.as_view(template_name="recuperar-senha.html"), name="recuperar-senha"),
     path("mailbox/", TemplateView.as_view(template_name="mailbox.html"), name="mailbox"),
+    path("escolher-email/", TemplateView.as_view(template_name="escolher-email.html"), name="escolher-email"),
+    path("escolher-email/", TemplateView.as_view(template_name="escolher-email.html"), name="escolher-email"),
     path("api/auth/",    include("usuarios.urls.auth")),
     path("api/usuario/", include("usuarios.urls.usuario")),
     path("api/info/",    include("usuarios.urls.info")),
