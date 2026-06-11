4305d91703b77b2e69df25994516763ea8571da4
Vinicius <viniciusprivado16@gmail.com>
2026-06-11 00:44:50 -0300

    Adicionado perfil

--- vopt/urls.py
+++ vopt/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("cadastro/", TemplateView.as_view(template_name="cadastro.html"), name="pagina_cadastro"),
     path("recuperar-senha/", TemplateView.as_view(template_name="recuperar-senha.html"), name="recuperar-senha"),
     path("mailbox/", TemplateView.as_view(template_name="mailbox.html"), name="mailbox"),
+    path("perfil/", TemplateView.as_view(template_name="perfil.html"), name="perfil"),
 
     path("api/auth/", include("usuarios.urls.auth")),
     path("api/usuario/", include("usuarios.urls.usuario")),
